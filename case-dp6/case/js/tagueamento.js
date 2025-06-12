73c9a5671608fd19beb478571e5a5d8fac9db16b
wmatiasp <wmatiasp@gmail.com>
2025-06-12 14:21:44 -0300

    Update tagueamento.js

--- case-dp6/case/js/tagueamento.js
+++ case-dp6/case/js/tagueamento.js
@@ -1,6 +1,2 @@
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+// Toda configuração foi feita através do Google Tag Manager junto com os screenshots de validação
+// Os arquivos foram disponibilizados no Github: https://github.com/wmatiasp/dp6teste
